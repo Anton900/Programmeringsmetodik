6d43c6af3c0ff70b56dd363a2c4b18e7d2ffb029
Anton Wallin <anton.wallin90@gmail.com>
2021-11-23 14:57:15 +0100

    fixes

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -180,42 +180,34 @@ int main()
     f(int_buffer(10));
 
     // 2
-    int size = 5;
-    int randArray[size];
-    for (int i = 0; i < size; i++)
-    {
-        randArray[i] = rand() % 100;
-    }
+    int_buffer buffer(5);
 
-    int randArray2[size];
-    for (int i = 0; i < size; i++)
-    {
-        randArray2[i] = rand() % 100;
-    }
+    generate_random_values(buffer);
+    int_sorted sort_buffer1(buffer.begin(), buffer.size());
 
-    int_sorted buffer1(randArray, size);
-    int_sorted buffer2(randArray2, size);
+    generate_random_values(buffer);
+    int_sorted sort_buffer2(buffer.end(), buffer.size());
 
-    std::cout << "\nbuffer1: ";
-    print_buffer(buffer1);
-    std::cout << "buffer2: ";
-    print_buffer(buffer2);
+    std::cout << "\nsort_buffer1: ";
+    print_buffer(sort_buffer1);
+    std::cout << "sort_buffer2: ";
+    print_buffer(sort_buffer2);
 
     std::cout << "\nBefore Insertion: ";
-    print_buffer(buffer1);
+    print_buffer(sort_buffer1);
     std::cout << "After insertion: ";
-    buffer1.insert(10);
-    buffer1.insert(70);
-    buffer1.insert(5);
-    print_buffer(buffer1);
+    sort_buffer1.insert(10);
+    sort_buffer1.insert(70);
+    sort_buffer1.insert(5);
+    print_buffer(sort_buffer1);
     std::cout << "\n";
 
-    buffer1 = buffer1.merge(buffer2);
+    sort_buffer1 = sort_buffer1.merge(sort_buffer2);
     std::cout << "Buffer1 After Merge: ";
-    print_buffer(buffer1);
+    print_buffer(sort_buffer1);
     std::cout << "\n";
 
-    auto buffer_sort = is_sorted(buffer1);
+    auto buffer_sort = is_sorted(sort_buffer1);
     if (buffer_sort)
         std::cout << "The buffer is sorted\n";
     else
